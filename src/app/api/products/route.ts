735431d4bd3c2567a3b7d987669f6161a27387ab
Boluwatife Oyekanmi <enitanboluwatife5@gmail.com>
2024-06-04 20:36:59 +0100

    added dynamic to api routes

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,6 +1,8 @@
 import { db } from "@/lib/config/db";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic"
+
 export async function GET(request: Request) {
   const url = new URL(request.url);
 
--- src/app/api/stack/route.ts
+++ src/app/api/stack/route.ts
@@ -1,10 +1,12 @@
 import { db } from "@/lib/config/db";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic"
+
 export async function GET(request: Request) {
   try {
     const stacks = await db.stacks.findMany();
-    return Response.json(stacks);
+    return NextResponse.json(stacks);
   } catch (error) {
     console.log(error);
   }
